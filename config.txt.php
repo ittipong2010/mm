2cd848845ce3d9bc1214bed54cd45b0f5fa07a55
ittipong <83745889+ittipong2010@users.noreply.github.com>
2022-07-15 14:35:06 +0700

    Update config.txt.php

--- config.txt.php
+++ config.txt.php
@@ -1,23 +1,31 @@
 <?php
-        $hostname="localhost";
-	$username="webbot";
-	$password="u25v!7Ns0";
-	$dbname="webbot";
-	$usertable="member";
-	$yourfield = "Username";
-	
-	mysql_connect($hostname,$username, $password) หรือเสียหาย ("html>script language='JavaScript'>alert('ไม่สามารถเชื่อมต่อกับฐานข้อมูลได้! โปรดลองอีกครั้งในภายหลัง'),history.go(-1)/script>/html>");
-	mysqli_select_db($dbname);
 
-$query = "SELECT * FROM $usertable";
-	
-	$result = mysqli_query($query);
-	
-	if($result){
-		while($row = mysqli_fetch_array($result)){
-			$name = $row["$yourfield"];
-			echo "Name: ".$name."br/>";
-		}
-	}
+error_reporting(0); // disable PHP errors, you can comment out or turn on logs if you need to fix issue
 
-?>
+/* Attempt MySQL server connection. Assuming you are running MySQL
+
+server with default setting (user 'root' with no password) */
+
+$link = mysqli_connect("localhost", "ittipong", "0a1_9gPh", "main");
+
+// Check connection status
+
+if ($link === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$logwebhook = ""; // discord webhook which receives login logs and keys created
+
+$adminwebhook = ""; // discord webhook which receives admin actions
+
+$webhookun = "KeyAuth Logs"; // webhook username
+
+$adminwebhookun = "KeyAuth Admin Logs"; // admin webhook's username
+
+$adminapikey = ""; // api key for api/admin (an api only my staff can use)
+
+$shoppyAPIkey = ""; // shoppy.gg API key for my staff to look up orders
+
+$proxycheckapikey = ""; // proxycheck.io API key to check if IP is considered a VPN
+
+$bunnyNetKey = ""; // bunny.net CDN used for custom domains
